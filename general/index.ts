20e9c1cad78268a5b5111e3a3694592ccd50c757
eli.b <eli.b@walkme.com>
2020-07-05 14:52:05 +0300

    add fake export

--- general/index.ts
+++ general/index.ts
@@ -1,3 +1,5 @@
-export * from './analytics';
-export * from './data';
-export * from './apps';
+  export * from './analytics';
+  export * from './data';
+  export * from './apps';
+
+  export const version = '0.0.1'
